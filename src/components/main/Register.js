f5d5ce21753df5378043f759c8578bb6054c828b
Kaiisea <abacatemarinheiro@gmail.com>
2022-06-30 16:02:09 +0200

    WORKING: register

--- src/components/main/Register.js
+++ src/components/main/Register.js
@@ -7,18 +7,18 @@ export function Register() {
       <form className={classes.form}>
         <h1 className={classes.formH1}>Join us, little Porocornio!</h1>
         <div className={classes.divDoubleInput}>
-        <input
-          id="first_name"
-          type="first_name"
-          placeholder="Name"
-          className={`${classes.field} ${classes.formSpacingRegister} ${classes.doubleInputOne}`}
-        />
-        <input
-          id="last_name"
-          type="last_name"
-          placeholder="Last name"
-          className={`${classes.field} ${classes.formSpacingRegister} ${classes.doubleInputTwo}`}
-        />
+          <input
+            id="first_name"
+            type="first_name"
+            placeholder="Name"
+            className={`${classes.field} ${classes.formSpacingRegister} ${classes.doubleInputOne}`}
+          />
+          <input
+            id="last_name"
+            type="last_name"
+            placeholder="Last name"
+            className={`${classes.field} ${classes.formSpacingRegister} ${classes.doubleInputTwo}`}
+          />
         </div>
         <div className={classes.divDoubleInput}>
           <input
@@ -27,21 +27,52 @@ export function Register() {
             placeholder="User name"
             className={`${classes.field} ${classes.formSpacingRegister} ${classes.doubleInputThree}`}
           />
-        <select id="pronouns" name="pronouns" className={classes.selectForm}>
-          <option value="he" className={classes.initialOptionForm}>Choose your pronouns</option>
-          <option value="she" className={classes.optionForm}>
-            She/her
-          </option>
-          <option value="he" className={classes.optionForm}>He/him</option>
-          <option value="they" className={classes.optionForm}>They/them</option>
-          <option value="sheTHey" className={classes.optionForm}>She/they</option>
-          <option value="heThey" className={classes.optionForm}>He/they</option>
-          <option value="ze" className={classes.optionForm}> Ze/zem</option>
-          <option value="it" className={classes.optionForm}>It/its</option>
-          <option value="xe" className={classes.optionForm}>Xe/xem</option>
-          <option value="nonConforming" className={classes.optionForm}>Pronouns Non-conforming</option>
-        </select>
+          <select id="pronouns" name="pronouns" className={classes.selectForm}>
+            <option value="he" className={classes.initialOptionForm}>
+              Choose your pronouns
+            </option>
+            <option value="she" className={classes.optionForm}>
+              She/her
+            </option>
+            <option value="he" className={classes.optionForm}>
+              He/him
+            </option>
+            <option value="they" className={classes.optionForm}>
+              They/them
+            </option>
+            <option value="sheTHey" className={classes.optionForm}>
+              She/they
+            </option>
+            <option value="heThey" className={classes.optionForm}>
+              He/they
+            </option>
+            <option value="ze" className={classes.optionForm}>
+              {" "}
+              Ze/zem
+            </option>
+            <option value="it" className={classes.optionForm}>
+              It/its
+            </option>
+            <option value="xe" className={classes.optionForm}>
+              Xe/xem
+            </option>
+            <option value="nonConforming" className={classes.optionForm}>
+              Pronouns Non-conforming
+            </option>
+          </select>
         </div>
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          className={`${classes.field}`}
+        />
+        <h5
+          className={`${classes.formSpacingRegister} ${classes.formPasswordRequirements}`}
+        >
+          Use a minimun 8-letters password with upper case, lower case and at least one digit
+        </h5>
         <input
           id="email"
           type="email"
@@ -52,12 +83,14 @@ export function Register() {
           id="phone"
           type="phone"
           placeholder="Telephone number"
+          list="+34"
           className={`${classes.field} ${classes.formSpacingRegister}`}
         />
         <input
-          id="email"
-          type="email"
-          placeholder="Email"
+          id="phone"
+          type="phone"
+          placeholder="Telephone number"
+          list="+34"
           className={`${classes.field} ${classes.formSpacingRegister}`}
         />
         <button type="submit" className={classes.button}>
